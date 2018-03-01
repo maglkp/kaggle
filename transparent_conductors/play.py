0fca680cb9be2e82c48b9561c1aaea348614cb53
Lukasz Koziol <lukasz.koziol@emc.com>
2018-03-01 14:01:45 +0000

    xgboost base

--- transparent_conductors/play.py
+++ transparent_conductors/play.py
@@ -11,16 +11,28 @@ from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 
 import xgboost as xgb
 
-def rmsle(y_true,y_pred):
+def rmsle(y_true, y_pred):
    assert len(y_true) == len(y_pred)
    return np.square(np.log(y_pred + 1) - np.log(y_true + 1)).mean() ** 0.5
 
+def train(target, X_train, y_train):		
+	model = xgb.XGBRegressor(seed=2017)
+	model.fit(X_train, y_train)
+	y_train_pred = model.predict(X_train)
+	print("RMSLE for " + target + ": " + str(rmsle(y_train, y_train_pred)))
+	return model
 
 X_train = pd.read_csv("train.csv")
-y_train = X_train['formation_energy_ev_natom']
-X_train.drop(['id', 'formation_energy_ev_natom', 'bandgap_energy_ev'], axis=1)
+formation_energy_ev_natom_train = X_train['formation_energy_ev_natom']
+bandgap_energy_ev_train = X_train['bandgap_energy_ev']
+X_train = X_train.drop(['id', 'formation_energy_ev_natom', 'bandgap_energy_ev'], axis=1)
 
-model = xgb.XGBRegressor(seed=2017)
-model.fit(X_train, y_train)
-y_train_pred = model.predict(X_train)
-rmsle(y_train, y_train_pred)
+model_f = train('formation_energy_ev_natom', X_train, formation_energy_ev_natom_train)
+model_b = train('bandgap_energy_ev', X_train, bandgap_energy_ev_train)
+
+out = pd.DataFrame()
+X_test = pd.read_csv("test.csv").drop(['id'], axis=1) 
+out['formation_energy_ev_natom'] = model_f.predict(X_test)
+out['bandgap_energy_ev'] = model_b.predict(X_test)
+out['id'] = range(1, 601)
+out.to_csv('test_predicion.csv', index=False)
