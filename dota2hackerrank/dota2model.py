0b7628470653d30455b683ca1667174f80fdb890
Lukasz Koziol <lukasz.koziol@emc.com>
2018-03-23 09:43:23 +0000

    initial impl

--- dota2hackerrank/dota2model.py
+++ dota2hackerrank/dota2model.py
@@ -7,13 +7,15 @@ seed = 2018
 np.random.seed(seed)
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score, f1_score
+from sklearn.metrics import accuracy_score
 
+import matplotlib.pyplot as plt
 import xgboost as xgb
 
 from hyperopt import hp
 
 TRAIN_FILE = 'trainingdata.txt'
+SAMPLE_FILE = 'trainingdata_sample.txt'
 TRAIN_LEN = 15000
 HEROES = ['Disruptor', 'Batrider', 'Naga Siren', 'Brewmaster', 'Lone Druid', 'Enigma', 'Windrunner',
           'Drow Ranger', 'Zeus', 'Earthshaker', 'Queen of Pain', 'Axe', 'Necrolyte', 'Lina', 'Phantom Assassin',
@@ -47,7 +49,6 @@ def get_column_names():
 
 def read_train_df():
     train = pd.DataFrame(0, index=np.arange(TRAIN_LEN), columns=get_column_names())
-    # Sven,Lone Druid,Venomancer,Clockwerk,Shadow Shaman,Invoker,Gyrocopter,Anti-Mage,Alchemist,Slark,2
     with open(TRAIN_FILE, 'r') as f:
         i = 0
         for line in f:
@@ -55,7 +56,7 @@ def read_train_df():
             team1 = [m + '1' for m in dat[0:5]]
             team2 = [m + '2' for m in dat[5:10]]
             members = team1 + team2
-            team1win = 1 if dat[-1] == '1' else 0
+            team1win = 1 if '1' in dat[-1] else 0
             row = train.iloc[i]
             for member in members:
                 row[member] = 1
@@ -64,4 +65,33 @@ def read_train_df():
     return train
 
 
-train = read_train_df()
+def train(X_train, y_train, X_test, y_test, show_importances=False, xgb_params={}):
+    # model = xgb.XGBRegressor(**xgb_params)
+    # model.feature_importances_
+
+    model = xgb.XGBRegressor(seed=2018)
+    model.fit(X_train, y_train)
+    y_train_pred = model.predict(X_train) > .5
+    print("Accuracy: " + str(accuracy_score(y_train, y_train_pred)))
+
+    if show_importances:
+        importances = model.feature_importances_
+        indices = np.argsort(importances)[::-1]
+        features = list(X_train)
+        plt.figure(figsize=(10, 5))
+        plt.title("Feature importances")
+        plt.bar(range(X_train.shape[1]), model.feature_importances_[indices], color="b", align="center")
+        plt.xticks(range(X_train.shape[1]), [features[x] for x in indices])
+        plt.xticks(rotation=90)
+        plt.xlim([-1, X_train.shape[1]])
+        plt.show()
+
+    return model
+
+train_data = read_train_df()
+y = train_data[['team1win']]
+X = train_data.drop(['team1win'], axis=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+train(X_train, y_train, X_test, y_test, True)
+
+
